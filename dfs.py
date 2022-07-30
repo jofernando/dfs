e50a3c795516d47fb66d9f139277d9e77eb8e1ec
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2022-07-29 21:38:16 -0300

    extende a classe para buscar o caminho entre 2 nos

--- dfs.py
+++ dfs.py
@@ -1,28 +1,14 @@
-class Graph:
-    def __init__(self, num_of_nodes, directed=True):
-        self.m_num_of_nodes = num_of_nodes
-        self.m_nodes = range(self.m_num_of_nodes)
-        self.m_directed = directed
-        self.m_adj_list = {node: set() for node in self.m_nodes}      
-	
-    def add_edge(self, node1, node2, weight=1):
-        self.m_adj_list[node1].add((node2, weight))
-        if not self.m_directed:
-            self.m_adj_list[node2].add((node1, weight))
-    
-    def print_adj_list(self):
-        for key in self.m_adj_list.keys():
-            print("node", key, ": ", self.m_adj_list[key])
+import networkx as nx
 
-    def dfs(self, start, target, path = [], visited = set()):
-        print(start, target)
+class Graph(nx.Graph):
+    def dfs_path(self, start, target, path = [], visited = set()):
         path.append(start)
         visited.add(start)
         if start == target:
             return path
-        for (neighbour, weight) in self.m_adj_list[start]:
+        for (neighbour) in self.adj[start]:
             if neighbour not in visited:
-                result = self.dfs(neighbour, target, path, visited)
+                result = self.dfs_path(neighbour, target, path, visited)
                 if result is not None:
                     return result
         path.pop()
