f92dfe15802b9f2ad0f5c15831b2b9b93b983124
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2022-07-29 23:03:04 -0300

    salva grafo com caminho

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from dfs import Graph
 from flask import Flask, render_template, request, send_file
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -12,13 +13,15 @@ def hello_world():
 @app.route('/enviar', methods=['POST'])
 def salvar():
     nodes = set()
-    nodes.update(request.form.getlist('emailCoautor[]'))
-    nodes.update(request.form.getlist('nomeCoautor[]'))
-    graph = nx.Graph()
-    for a, b in zip(request.form.getlist('emailCoautor[]'), request.form.getlist('nomeCoautor[]')):
-        graph.add_edge(int(a), int(b))
-    nx.draw(graph, with_labels=True, font_weight='bold')
+    route_edges = [(path[n], path[n+1]) for n in range(len(path)-1)]
+    pos = nx.spring_layout(graph)
+    nodes = [0,1,2,3,4]
+    nx.draw_networkx_nodes(graph, pos=pos, nodelist=nodes, node_color=[(0.8,0.4,0.9),'y','y','y','g'])
+    nx.draw_networkx_labels(graph, pos=pos)
+    nx.draw_networkx_edges(graph, pos=pos, edgelist=graph.edges)
+    nx.draw_networkx_edges(graph, pos=pos, edgelist=route_edges, edge_color = 'r')
     filename = datetime.now().strftime("graphs/%Y%m%d%H%M%S") + '.png'
+    plt.legend(['partida', '', 'caminho', 'destino'])
     plt.savefig(filename)
     plt.clf()
     return send_file(filename, mimetype='image/gif')
